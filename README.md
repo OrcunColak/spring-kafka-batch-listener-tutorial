09890832d90dceb43f9a493357f9c50df115d5e4
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-25 10:00:49 +0300

    upgrade spring

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 The original idea is from  
 https://towardsdev.com/kafka-batch-message-listener-in-spring-boot-caa70747deee
+
+We can configure Spring Kafka Listener in two ways
+
+1. By using Spring @KafkaListener annotation
+2. By using Spring ConcurrentKafkaListenerContainerFactory
--- src/main/java/com/colak/springkafkaembeddedtesttutorial/service/KafkaConsumer.java
+++ src/main/java/com/colak/springkafkaembeddedtesttutorial/service/KafkaConsumer.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,12 +16,13 @@ import java.util.List;
 public class KafkaConsumer {
 
     @KafkaListener(topics = "topic1", groupId = "group1", batch = "true")
-    public void receive(List<ConsumerRecord<String, String>> consumerRecords) {
+    public void receive(List<ConsumerRecord<String, String>> consumerRecords, Acknowledgment acknowledgment) {
         log.info("List size : {}", consumerRecords.size());
 
         for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
             log.info("Values : {}", consumerRecord.value());
         }
 
+        acknowledgment.acknowledge();
     }
 }
